44a21f248c07ee8efca86e4c4253fb75df70bbc1
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-05 17:21:58 -0800

    add use-package settings

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,13 +1,34 @@
 ; ~/.emacs.d/init.el
 
-; package system
-(require 'cask "/usr/local/share/emacs/site-lisp/cask.el")
+; load-path setting for cask
+(setq load-path
+  (append
+   (list nil
+         "~/.cask" ; install from script
+         "/usr/local/share/emacs/site-lisp" ; install from homebrew
+         )
+   load-path
+   ))
+
+; Package management tool
+; see https://github.com/cask/cask
+(require 'cask)
 (cask-initialize)
 
+; Cask file management tool
+; see https://github.com/rdallasgray/pallet
 (require 'pallet)
-;(pallet-mode t)
+(pallet-mode t)
+
+; use-package
+; see https://github.com/jwiegley/use-package
+;     http://qiita.com/kai2nenobu/items/5dfae3767514584f5220
+(unless (require 'use-package nil t)
+  (defmacro use-package (&rest args)))
 
 ; init-loader
-(require 'init-loader)
-(setq init-loader-show-log-after-init nil)
-(init-loader-load "~/.emacs.d/inits")
+; see https://github.com/emacs-jp/init-loader
+(use-package init-loader
+  :config
+  (setq init-loader-show-log-after-init t)
+  (init-loader-load "~/.emacs.d/inits"))
