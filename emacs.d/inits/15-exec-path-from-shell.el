490151184c14c4394ea107ade68081665a5672f9
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-05 17:30:14 -0800

    modify find-file keybind

--- emacs.d/inits/15-exec-path-from-shell.el
+++ emacs.d/inits/15-exec-path-from-shell.el
@@ -1,2 +1,4 @@
 ; use shell environment variable PATH
+; see https://github.com/purcell/exec-path-from-shell
+; see http://d.hatena.ne.jp/syohex/20130718/1374154709
 (exec-path-from-shell-initialize)
--- emacs.d/inits/99-keybinds.el
+++ emacs.d/inits/99-keybinds.el
@@ -13,6 +13,16 @@
 (define-key global-map (kbd "M-x") 'helm-M-x)
 (define-key global-map (kbd "C-x b") 'helm-buffers-list)
 
+; helm-find-file
+; see http://d.hatena.ne.jp/a_bicky/20140104/1388822688
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "?") 'helm-select-action)
+(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
+  "Execute command only if CANDIDATE exists"
+  (when (file-exists-p candidate)
+        ad-do-it))
+
 ;; etags
 (define-key global-map (kbd "C-c t") 'syohex/helm-etags-select)
 (define-key global-map (kbd "C-c b") 'pop-tag-mark)
