5193f82e61c8d066fdd8085be8cc5684f03fef20
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-06 19:27:36 -0800

    add git-gutter setting

--- emacs.d/inits/16-git.el
+++ emacs.d/inits/16-git.el
@@ -1,6 +1,14 @@
 ; git client
-
 (use-package magit
   :config
   (global-set-key (kbd "C-c g") 'magit-status)
+  ; see http://stackoverflow.com/questions/23802485/what-is-emacs-mrev-mode
+  (magit-auto-revert-mode -1)
+  )
+
+; see https://github.com/syohex/emacs-git-gutter
+;     http://emacs-jp.github.io/packages/vcs/git-gutter.html
+(use-package git-gutter
+  :config
+  (global-git-gutter-mode t)
   )
