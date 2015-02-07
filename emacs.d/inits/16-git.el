9550572484d11674d0d2bdfdc1904619c93b6ef4
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-06 19:38:18 -0800

    modify magit emacsclient

--- emacs.d/inits/16-git.el
+++ emacs.d/inits/16-git.el
@@ -4,6 +4,7 @@
   (global-set-key (kbd "C-c g") 'magit-status)
   ; see http://stackoverflow.com/questions/23802485/what-is-emacs-mrev-mode
   (magit-auto-revert-mode -1)
+  (set-variable 'magit-emacsclient-executable "/usr/local/bin/emacsclient")
   )
 
 ; see https://github.com/syohex/emacs-git-gutter
--- emacs.d/inits/cocoa-emacs-magit.el
+++
@@ -1,1 +0,0 @@
-(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
