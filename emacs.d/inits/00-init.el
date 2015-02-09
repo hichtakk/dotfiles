5c9109a6d8f5119f37708c983c420df99b240205
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-09 12:26:40 -0800

    change directry and file name for ac and backup

--- emacs.d/inits/00-init.el
+++ emacs.d/inits/00-init.el
@@ -33,12 +33,12 @@
 (global-auto-revert-mode t)
 
 ; see http://qiita.com/catatsuy/items/f9fad90fa1352a4d3161
-;; create backup file in ~/.emacs.d/backup
+;; create backup file in ~/.emacs.d/.backup
 (setq make-backup-files t)
 (setq backup-directory-alist
-      (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/backup"))
+      (cons (cons "\\.*$" (expand-file-name "~/.emacs.d/.backup"))
             backup-directory-alist))
 
-;; create auto-save file in ~/.emacs.d/backup
+;; create auto-save file in ~/.emacs.d/.backup
 (setq auto-save-file-name-transforms
-            `((".*" ,(expand-file-name "~/.emacs.d/backup/") t)))
+            `((".*" ,(expand-file-name "~/.emacs.d/.backup/") t)))
--- emacs.d/inits/11-auto-complete.el
+++ emacs.d/inits/11-auto-complete.el
@@ -1,11 +1,10 @@
 ; auto-complete
 
-(when (locate-library "auto-complete")
-  (require 'auto-complete-config)
-
+(use-package auto-complete-config
+  :config
   (ac-config-default)
   (setq ac-dwim t)
-
+  (setq ac-comphist-file "~/.emacs.d/.ac-comphist.dat")
   (define-key ac-completing-map (kbd "C-n") 'ac-next)
   (define-key ac-completing-map (kbd "C-p") 'ac-previous)
   (define-key ac-completing-map (kbd "TAB") 'ac-complete)
