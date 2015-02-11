3df4a4eb44fe82c239a5516225013a1f9b05cdb9
Hirotaka Ichikawa <hichtakk@gmail.com>
2015-02-10 17:03:46 -0800

    modify mode-line design

--- emacs.d/inits/01-mode-line.el
+++ emacs.d/inits/01-mode-line.el
@@ -1,7 +1,7 @@
 ; Mode line settings
 
 ;; show current function name in modeline
-(which-function-mode t)
+;(which-function-mode t)
 
 ; powerline
 ; see https://github.com/milkypostman/powerline
@@ -9,7 +9,6 @@
 
 ; powerline custom theme
 (defun powerline-my-theme ()
-  "Setup a my-powerline theme based on powerline-center-theme"
   (interactive)
   (setq-default mode-line-format
                 '("%e"
@@ -27,37 +26,57 @@
                           (lhs (list (powerline-raw "%*" nil 'l)
                                      (powerline-buffer-size nil 'l)
                                      (powerline-buffer-id nil 'l)
-                                     (when (and (boundp 'which-function-mode) which-function-mode)
-                                       (powerline-raw which-func-format nil 'l))
                                      (powerline-raw " ")
                                      (funcall separator-left mode-line face1)
-                                     (powerline-narrow face1 'l)
-                                     (powerline-vc face1)))
-                          (rhs (list (powerline-raw global-mode-string face1 'r)
-                                     (powerline-raw "%4l" face1 'r)
-                                     (powerline-raw ":" face1)
-                                     (powerline-raw "%3c" face1 'r)
-                                     (funcall separator-right face1 mode-line)
-                                     (powerline-raw " ")
-                                     (powerline-raw "%6p" nil 'r)
-                                     (powerline-hud face2 face1)))
-                          (center (list (powerline-raw " " face1)
-                                        (funcall separator-left face1 face2)
-                                        (when (boundp 'erc-modified-channels-object)
-                                          (powerline-raw erc-modified-channels-object face2 'l))
-                                        (powerline-major-mode face2 'l)
-                                        (powerline-process face2)
-                                        (powerline-raw " :" face2)
-                                        (powerline-minor-modes face2 'l)
-                                        (powerline-raw " " face2)
-                                        (funcall separator-right face2 face1))))
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format face2 'l))
+                                     (powerline-minor-modes face2 'l)
+                                     (powerline-narrow face2 'l)
+                                     (powerline-raw " " face2)
+                                     ))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (powerline-raw " " face1)
+                                     (powerline-vc face1 'r)
+                                     (funcall separator-right face1 nil)
+                                     (powerline-raw "%4l" nil 'l)
+                                     (powerline-raw ":" nil 'l)
+                                     (powerline-raw "%3c " nil 'r)
+                                     (powerline-hud face2 face1))))
                      (concat (powerline-render lhs)
-                             (powerline-fill-center face1 (/ (powerline-width center) 2.0))
-                             (powerline-render center)
-                             (powerline-fill face1 (powerline-width rhs))
+                             (powerline-fill face2 (powerline-width rhs))
                              (powerline-render rhs)))))))
+
 
 (use-package powerline
   :config
   (powerline-my-theme)
+  ; customize color
+  (set-face-attribute 'mode-line nil
+                      :foreground "#FFFACD"
+                      :background "#3A539B"
+                      :box nil)
+  (set-face-attribute 'powerline-active1 nil
+                      :background "#4682B4"
+                      :inherit 'mode-line)
+  (set-face-attribute 'powerline-active2 nil
+                      :background "#5F5F5F"
+                      :inherit 'mode-line)
+
+  (set-face-attribute 'mode-line-inactive nil
+                      :foreground "#FFFACD"
+                      :background "#333333"
+                      :box nil)
+  (set-face-attribute 'powerline-inactive1 nil
+                      :background "#5F5F5F"
+                      :inherit 'mode-line)
+  (set-face-attribute 'powerline-inactive2 nil
+                      :background "#808080"
+                      :inherit 'mode-line)
   )
