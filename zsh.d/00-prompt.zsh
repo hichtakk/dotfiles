fab0bfdaf7deb1aada42f2f2129fc33ada20a0b9
Hirotaka <hichtakk@gmail.com>
2017-05-13 12:44:20 -0700

    prompt vcs-info

--- zsh.d/00-prompt.zsh
+++ zsh.d/00-prompt.zsh
@@ -1,5 +1,5 @@
 # set prompt
-RPROMPT="[%~]"
+#RPROMPT="[%~]"
 PROMPT2="%{${fg[red]}%}%_%%%{${reset_color}%} "
 SPROMPT="%{${fg[red]}%}%r is correct? [n,y,a,e]:%{${reset_color}%} "
 [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] && 
--- zsh.d/10-vcs.zsh
+++
@@ -1,8 +0,0 @@
-# vcs_info
-## see http://qiita.com/mollifier/items/8d5a627d773758dd8078
-autoload -Uz vcs_info
-zstyle ':vcs_info:*' formats \
-       '(%{%F{white}%K{green}%}%s%{%f%k%})-[%{%F{white}%K{blue}%}%b%{%f%k%}]'
-zstyle ':vcs_info:*' actionformats \
-       '(%{%F{white}%K{green}%}%s%{%f%k%})-[%{%F{white}%K{blue}%}%b%{%f%k%}|%{%white}%K{red}%}%a%{%f%k%}]'
-RPROMPT="%1(v|%F{green}%1v%f|)"
